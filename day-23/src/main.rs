2f51cbbab49ed633dabc263ac90e3922cf2f5011
Kevan Hollbach <khollbach@gmail.com>
2026-06-11 15:40:05 -0400

    Can't get away with just checking corners...

--- day-23/src/main.rs
+++ day-23/src/main.rs
@@ -1,17 +1,18 @@
-use std::{cmp::Reverse, io, ops::Sub};
+use std::{
+    io,
+    ops::{Add, Mul, Sub},
+};
 
 use anyhow::Result;
-use itertools::Itertools;
-use rayon::prelude::*;
 
 mod input;
-mod part_2_faster;
+mod part_2;
 
 fn main() -> Result<()> {
     let nanobots = input::parse(io::stdin().lock())?;
     let ans = part_1(&nanobots);
     println!("{}", ans);
-    let ans = part_2(&nanobots);
+    let ans = part_2::solve(&nanobots);
     println!("{}", ans);
     Ok(())
 }
@@ -30,78 +31,6 @@ fn part_1(nanobots: &[Nanobot]) -> i32 {
     count
 }
 
-/*
-part 2 brainstorming
-- n=1,000 -- O(n^3) feasible
-- size of #s =~ 10,000,000
-
-brute force idea -- O(n^4), but fck it let's try anyways
-- project all cubes onto each axis; getting a list of all
-    - x border points
-    - y border points
-    - z border points
-- for each x,y,z from those sets, try that candidate point
-    (can maybe use rayon for multi-core)
-- take the overall best candidate
-
-- 5 seconds for N=100
-    - so maybe 50,000 seconds for N=1000 ? -- which could probably finish overnight
-- 24 seconds for N=300
-- 2 mins, 26 seconds for N=500
-    - so maybe only 40 mins for N=1000 ??
-
----
-
-idea for speeding this up:
-- first try all cube corners, and then use the max hit-count of those
-    as a lower bound on the actual answer -- call it L
-- this should let us avoid checking all border points as follows:
-    - take the projection of the cubes onto the x axis
-    - find the intervals that have hit-count at least L
-        (linear time is possible, but not needed -- can do an n^2 thing here)
-        (the hope is that this cuts down the # of border points)
-    - (similar for y and z)
-    - then feed these reduced x/y/z lists into the existing brute force algo
-
----
-
-Wait a SECOND. I'm trolling. These are not even cubes.
-But maybe we could apply a transformation (rotation?) to the input space
-so that they become cubes?
-... nope. they've got only 6 pointy bits, not 8...
-- they're kinda like two 4-sided pyramids glued together. I'm sure there's a term for this.
-*/
-
-fn part_2(nanobots: &[Nanobot]) -> i32 {
-    let (xs, ys, zs): (Vec<_>, Vec<_>, Vec<_>) = nanobots
-        .iter()
-        .flat_map(|n| {
-            let Point { x, y, z } = n.pos;
-            let r = n.range;
-            [(x - r, y - r, z - r), (x + r, y + r, z + r)]
-        })
-        .multiunzip();
-
-    let xs = xs.into_par_iter();
-    let ys = ys.into_par_iter();
-    let zs = zs.into_par_iter();
-
-    let best = xs
-        .flat_map(move |x| {
-            let zs = zs.clone();
-            ys.clone()
-                .flat_map(move |y| zs.clone().map(move |z| Point { x, y, z }))
-        })
-        .max_by_key(|&p| (hit_count(nanobots, p), Reverse(p.manhattan_norm())))
-        .unwrap();
-
-    best.manhattan_norm()
-}
-
-fn hit_count(nanobots: &[Nanobot], p: Point) -> usize {
-    nanobots.par_iter().filter(|n| n.in_range(p)).count()
-}
-
 #[derive(Debug, Clone, Copy)]
 struct Nanobot {
     pos: Point,
@@ -112,6 +41,19 @@ struct Nanobot {
 impl Nanobot {
     fn in_range(self, p: Point) -> bool {
         self.pos.manhattan_distance(p) <= self.range
+    }
+
+    fn corners(self) -> [Point; 6] {
+        let p = self.pos;
+        let r = self.range;
+        [
+            p + Point::X * r,
+            p + Point::X * -r,
+            p + Point::Y * r,
+            p + Point::Y * -r,
+            p + Point::Z * r,
+            p + Point::Z * -r,
+        ]
     }
 }
 
@@ -130,6 +72,22 @@ impl Point {
     fn manhattan_norm(self) -> i32 {
         self.x.abs() + self.y.abs() + self.z.abs()
     }
+
+    const X: Self = Self { x: 1, y: 0, z: 0 };
+    const Y: Self = Self { x: 0, y: 1, z: 0 };
+    const Z: Self = Self { x: 0, y: 0, z: 1 };
+}
+
+impl Add for Point {
+    type Output = Self;
+
+    fn add(self, other: Self) -> Self {
+        Self {
+            x: self.x + other.x,
+            y: self.y + other.y,
+            z: self.z + other.z,
+        }
+    }
 }
 
 impl Sub for Point {
@@ -140,6 +98,18 @@ impl Sub for Point {
             x: self.x - other.x,
             y: self.y - other.y,
             z: self.z - other.z,
+        }
+    }
+}
+
+impl Mul<i32> for Point {
+    type Output = Point;
+
+    fn mul(self, scale: i32) -> Self {
+        Self {
+            x: self.x * scale,
+            y: self.y * scale,
+            z: self.z * scale,
         }
     }
 }
---
+++ day-23/src/part_2.rs
@@ -0,0 +1,76 @@
+/*
+part 2 brainstorming
+- n=1,000 -- O(n^3) feasible
+- size of #s =~ 10,000,000
+
+brute force idea -- O(n^4), but fck it let's try anyways
+- project all cubes onto each axis; getting a list of all
+    - x border points
+    - y border points
+    - z border points
+- for each x,y,z from those sets, try that candidate point
+    (can maybe use rayon for multi-core)
+- take the overall best candidate
+
+- 5 seconds for N=100
+    - so maybe 50,000 seconds for N=1000 ? -- which could probably finish overnight
+- 24 seconds for N=300
+- 2 mins, 26 seconds for N=500
+    - so maybe only 40 mins for N=1000 ??
+
+---
+
+idea for speeding this up:
+- first try all cube corners, and then use the max hit-count of those
+    as a lower bound on the actual answer -- call it L
+- this should let us avoid checking all border points as follows:
+    - take the projection of the cubes onto the x axis
+    - find the intervals that have hit-count at least L
+        (linear time is possible, but not needed -- can do an n^2 thing here)
+        (the hope is that this cuts down the # of border points)
+    - (similar for y and z)
+    - then feed these reduced x/y/z lists into the existing brute force algo
+
+---
+
+Wait a SECOND. I'm trolling. These are not even cubes.
+But maybe we could apply a transformation (rotation?) to the input space
+so that they become cubes?
+... nope. they've got only 6 pointy bits, not 8...
+- they're kinda like two 4-sided pyramids glued together. I'm sure there's a term for this.
+
+---
+
+Let's just try corners-only and see what the online judge says :)
+    [day-23/src/part_2.rs:62:5] best = Point {
+        x: 62699654,
+        y: 21730841,
+        z: 24154493,
+    }
+    [day-23/src/part_2.rs:63:5] hit_count(nanobots, best) = 894
+    ans: 108584988 (manhattan norm)
+verdict: "not right; too high"
+*/
+
+use std::cmp::Reverse;
+
+use rayon::prelude::*;
+
+use crate::{Nanobot, Point};
+
+pub fn solve(nanobots: &[Nanobot]) -> i32 {
+    let corners: Vec<_> = nanobots.iter().flat_map(|n| n.corners()).collect();
+
+    let best = corners
+        .into_par_iter()
+        .max_by_key(|&p| (hit_count(nanobots, p), Reverse(p.manhattan_norm())))
+        .unwrap();
+
+    dbg!(best);
+    dbg!(hit_count(nanobots, best));
+    best.manhattan_norm()
+}
+
+fn hit_count(nanobots: &[Nanobot], p: Point) -> usize {
+    nanobots.par_iter().filter(|n| n.in_range(p)).count()
+}
